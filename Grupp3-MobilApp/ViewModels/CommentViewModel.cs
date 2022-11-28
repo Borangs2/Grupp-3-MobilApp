d8a9330271332821c96374e4601a5088ad18567e
Pierre Åkerman <pirreakerman@gmail.com>
2022-11-28 11:15:16 +0100

    Changed errand.comments to list of CommentViewModel to be able to show the name of the comment author.

--- Grupp3-MobilApp/ViewModels/CommentViewModel.cs
+++ Grupp3-MobilApp/ViewModels/CommentViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupp3_MobilApp.Models;
 
 namespace Grupp3_MobilApp.ViewModels
 {
-    internal class CommentViewModel
+    public class CommentViewModel
     {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public TechnicianModel Author { get; set; }
+        public DateTime PostedAt { get; set; }
     }
 }
